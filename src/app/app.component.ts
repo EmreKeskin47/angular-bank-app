c39111b1cf5f47fdfbe8a91b13902803fe31a898
EmreKeskin47 <43786018+EmreKeskin47@users.noreply.github.com>
2021-01-24 23:41:25 +0300

    implemented indexedDB, only has users table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Dexie from 'dexie';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bank-app';
+  db: any;
+  newUser!: User;
+  rows: User[] = [];
+
+  ngOnInit() {
+    this.makeDatabase();
+    this.connectToDatabase();
+  }
+
+  makeDatabase(): void {
+    this.db = new Dexie('BankDatabase');
+    this.db.version(1).stores({
+      users: 'name, password',
+    });
+    this.loadRows();
+  }
+
+  connectToDatabase(): void {
+    this.db.open().catch((error: string) => {
+      alert('Errod during connecting to database : ' + error);
+    });
+  }
+
+  clearRows(): void {
+    this.db.users.clear().then((result: any) => console.log(result));
+    this.loadRows();
+  }
+
+  loadRows(): void {
+    this.db.users.toArray().then((p: any) => (this.rows = p));
+  }
+
+  addRow(user: User): void {
+    this.db.users.add({
+      name: user.name,
+      password: user.password,
+    });
+
+    this.loadRows();
+    this.newUser = new User('test', 'test');
+  }
 }
