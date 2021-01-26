215d88e38751cf82c94d4a539bbafec1c3abcd36
EmreKeskin47 <43786018+EmreKeskin47@users.noreply.github.com>
2021-01-26 21:35:30 +0300

    database functions are moved to seperate service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+import { UserService } from './services/user.service';
 import { Component } from '@angular/core';
 import Dexie from 'dexie';
-import { User } from './models/user';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
@@ -9,45 +10,11 @@ import { User } from './models/user';
 })
 export class AppComponent {
   title = 'bank-app';
-  db: any;
-  newUser!: User;
-  rows: User[] = [];
+
+  constructor(private userDbService: UserService) {}
 
   ngOnInit() {
-    this.makeDatabase();
-    this.connectToDatabase();
-  }
-
-  makeDatabase(): void {
-    this.db = new Dexie('BankDatabase');
-    this.db.version(1).stores({
-      users: 'name, password',
-    });
-    this.loadRows();
-  }
-
-  connectToDatabase(): void {
-    this.db.open().catch((error: string) => {
-      alert('Errod during connecting to database : ' + error);
-    });
-  }
-
-  clearRows(): void {
-    this.db.users.clear().then((result: any) => console.log(result));
-    this.loadRows();
-  }
-
-  loadRows(): void {
-    this.db.users.toArray().then((p: any) => (this.rows = p));
-  }
-
-  addRow(user: User): void {
-    this.db.users.add({
-      name: user.name,
-      password: user.password,
-    });
-
-    this.loadRows();
-    this.newUser = new User('test', 'test');
+    this.userDbService.makeDatabase();
+    this.userDbService.connectToDatabase();
   }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import Dexie from 'dexie';
+import { User } from '../models/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  db: any;
+  newUser!: User;
+  rows: User[] = [];
+
+  makeDatabase(): void {
+    this.db = new Dexie('BankDatabase');
+    this.db.version(1).stores({
+      users: 'name, password',
+    });
+    this.loadRows();
+  }
+
+  connectToDatabase(): void {
+    this.db.open().catch((error: string) => {
+      alert('Errod during connecting to database : ' + error);
+    });
+  }
+
+  clearRows(): void {
+    this.db.users.clear().then((result: any) => console.log(result));
+    this.loadRows();
+  }
+
+  loadRows(): void {
+    this.db.users.toArray().then((p: any) => {
+      this.rows = p;
+    });
+  }
+
+  addRow(user: User): void {
+    this.db.users.add({
+      name: user.name,
+      password: user.password,
+    });
+
+    this.loadRows();
+    this.newUser = new User('test', 'test');
+  }
+
+  authUser(username: string, password: string): { [key: string]: any } {
+    let nameExist = false;
+    let passCorrect = false;
+    this.db.users.toArray().then((row: any) => {
+      var i;
+      for (i = 0; i < row.length; i++) {
+        if (row[i].name === username) {
+          nameExist = true;
+          if (row[i].password == password) {
+            passCorrect = true;
+          }
+        }
+      }
+    });
+    return {
+      nameCheck: nameExist,
+      passCheck: passCorrect,
+    };
+  }
+}
